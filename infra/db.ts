46b503fab58991474c2d17d5ddd396072c4dbc92
Anderson Clavico Moreira <anderson.clavico@xpi.com.br>
2019-10-29 08:07:36 -0300

    CORS

--- infra/db.ts
+++ infra/db.ts
@@ -1,7 +1,7 @@
 import * as mongoose from "mongoose";
 
 class Database{
-    private DB_URL = "mongodb://link-db/db_portal";
+    private DB_URL = "mongodb://localhost:32773/db_portal";
 
     createConnection(){
         mongoose.connect(this.DB_URL);
