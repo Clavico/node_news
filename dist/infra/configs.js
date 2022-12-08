e115d3adfe8fbc3e42c0d5fe23457b0a23179a40
Anderson Clavico <short_acm@hotmail.com>
2022-12-08 20:02:48 -0300

    clear

--- dist/infra/configs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Configs {
-    constructor() {
-        this.secret = "short short short";
-    }
-}
-exports.default = new Configs;
